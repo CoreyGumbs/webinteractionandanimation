e5912c8a8bc36ed75598b76fd20221c87f2c4134
Sonoflight <coreygumbs@gmail.com>
2015-07-09 23:40:39 -0400

    Homework1 Final Update

--- Gumbs_Corey_Homework1/js/main.js
+++ Gumbs_Corey_Homework1/js/main.js
@@ -42,7 +42,7 @@ Reminder - set the style first then draw.
 //Draw Rectangle here
 
 
-	
+	// call to canvas and 2d context
 	var theCanvas = document.getElementById("Canvas1");
 	var ctx = theCanvas.getContext("2d");
 	
@@ -53,9 +53,9 @@ Reminder - set the style first then draw.
 	ctx.lineWidth = 5;
 
 	//Drawing of rectangle
-
-	ctx.strokeRect(0, 0, 50, 100);
 	ctx.fillRect(0, 0, 50, 100);
+	ctx.strokeRect(0, 0, 50, 100);
+	
 	
 	
 
@@ -75,9 +75,8 @@ Use the arc method
 
 //Draw Circle here
 
-//if(Modernizr.canvas){
-	//Canvas is supported
-	
+
+	// call to canvas and 2d context
 	var theCanvas2 = document.getElementById("Canvas2");
 	var ctx2 = theCanvas2.getContext("2d");
 	
@@ -111,6 +110,8 @@ Height and width and color are up to you.
 
 //Draw Star here
 	
+
+	// call to canvas and 2d context
 	var theCanvas3 = document.getElementById("Canvas3");
 	var ctx3 = theCanvas3.getContext("2d");
 	
@@ -120,15 +121,19 @@ Height and width and color are up to you.
 	ctx3.fillStyle = "red";
 	ctx3.lineWidth = 8;
 	
-	//Star begins
-
+	//Star line drawings begins
 	ctx3.lineJoin = "miter";
 	ctx3.beginPath();
 	ctx3.moveTo(100, 100);
+	//line from top of star to lower right corner
 	ctx3.lineTo(125, 250);
+	//line from lower right corner of star to left corner 
 	ctx3.lineTo(30, 150);
+	//straight line from left corner of star to right corner of star
 	ctx3.lineTo(150, 150);
+	//line from right corner of star to bottom left corner of star
 	ctx3.lineTo(30, 250);
+	//closes the line paths of star
 	ctx3.closePath();
 	ctx3.fill();
 	ctx3.stroke();
@@ -152,6 +157,7 @@ Do not overlap any other object.
 
 //Draw Umbrella top here
 
+	// call to canvas and 2d context
 	var theCanvas4 = document.getElementById("Canvas4");
 	var ctx4 = theCanvas4.getContext("2d");
 
@@ -162,6 +168,7 @@ Do not overlap any other object.
 
 	//position elements to center of canvas
 	ctx4.translate(ctx4.canvas.width/2, ctx4.canvas.height/2);
+	
 	//1/2 circle/Arc
 	ctx4.beginPath();
 	ctx4.arc(0,0, 200, 0, 1 * Math.PI, true);
@@ -171,7 +178,7 @@ Do not overlap any other object.
 	ctx4.bezierCurveTo(-200, -20, -10, -50, 0, 0);
 	ctx4.moveTo(0,0);
 	ctx4.bezierCurveTo(0, 0, 0, -50, 200, 0);
-	
+
 	//fill in all elements
 	ctx4.fill();
 	ctx4.stroke();	
@@ -180,11 +187,31 @@ Do not overlap any other object.
 PART 5
 
 Practice using text.
-Draw text into your canvas.  It can said whatever you would like in any color.
+Draw text into your canvas.  It can say whatever you would like in any color.
 
 ********************************************/
 
 //Draw text here
+
+// call to canvas and 2d context
+var theCanvas5 = document.getElementById("Canvas5");
+var ctx5 = theCanvas5.getContext("2d");
+
+//text string variable
+var myText = "This project has been kicking my butt!";
+
+
+//Declaring Styles
+ctx5.font = "25pt Verdana";
+ctx5.fillStyle = "#01708c";
+ctx5.strokeStyle = "rgba(255,0,0, .3)";
+ctx5.lineWidth = 3;
+
+//draws text to canvas
+ctx5.fillText(myText, 50, 100);
+
+//adds stroke outline to text
+ctx5.strokeText(myText, 50, 160);
 
 /*******************************************
 PART 6
@@ -201,6 +228,24 @@ Reminder to use the drawImage method for all 3 of the ways.
 
 //Draw images here
 
+//begins onload images into DOM - continues into Canvas 7
+window.onload = function(){
+// call to canvas and 2d context
+var theCanvas6 = document.getElementById("Canvas6");
+var ctx6 = theCanvas6.getContext("2d");
+
+//variable to hold image
+var srcImg = document.getElementById('logo');
+
+//original image
+ctx6.drawImage(srcImg, 0,0);
+
+//scaled image to 50%
+ctx6.drawImage(srcImg, 0, 1100, 1650, 544);
+
+//spliced image
+ctx6.drawImage(srcImg, 305, 582, 330, 600, 150, 1700, 200, 350);
+
 
 
 /*******************************************
@@ -210,14 +255,111 @@ Putting it all together.
 
 Using a combination of all the methods. 
 Create a complex scene.
-You must use at least 3 different methods.
-
-********************************************/
+You must 
+*********************use at least 3 different methods.
+***********************/
 
 // Draw scene here
 
 
+	// call to canvas and 2d context
+var theCanvas7 = document.getElementById("Canvas7");
+var ctx7 = theCanvas7.getContext("2d");
 
+
+
+/**************  Images *******************/
+
+var srcImg2 = document.getElementById('sky');
+
+//background image
+ctx7.drawImage(srcImg2, 0,0, 1000, 500);
+
+
+
+/*********** Circle/Sun ***************/
+
+
+//Declaring Styles for Circle
+	ctx7.save();
+
+	ctx7.strokeStyle = "#ffa800";
+	ctx7.fillStyle = "rgba(252, 255, 0, .9)";
+	ctx7.lineWidth = 2;
+	ctx7.shadowColor = "#ffa800";
+	ctx7.shadowOffsetX = 0;
+	ctx7.shadowOffsetY = 0;
+	ctx7.shadowBlur = 50;
+
+	//full circle
+	ctx7.beginPath();
+	ctx7.arc(150, 100, 50, 0, 2 * Math.PI);
+	ctx7.fill();
+	ctx7.stroke();	
+
+	ctx7.restore();
+
+
+
+/************** Text *********************/
+ctx7.save();
+//text string variable
+var webText = "Web Interaction";
+var animateText = "Animation";
+
+//Declaring Styles for text
+ctx7.font = "40pt Verdana";
+ctx7.fillStyle = "red";
+ctx7.strokeStyle = "rgba(255,0,0, .3)";
+ctx7.lineWidth = 3;
+
+//draws text to canvas
+ctx7.fillText(webText, 350, 100);
+ctx7.fillText(animateText, 450, 180);
+
+ctx7.restore();
+
+/*****************  lines/ teepee ******************/
+
+
+ctx7.save()
+//Declaring Styles for lines
+
+	ctx7.strokeStyle = "rgb(222,184,135)";
+	ctx7.fillStyle = "rgb(245,222,179)";
+	ctx7.lineWidth = 8;
+	
+	//Star line drawings begins
+	ctx7.lineJoin = "miter";
+	ctx7.beginPath();
+	ctx7.moveTo(700, 250);
+	ctx7.lineTo(650, 400);
+	ctx7.lineTo(750, 400);
+
+	//closes the line path
+	ctx7.closePath();
+	ctx7.fill();
+	ctx7.stroke();
+
+ctx7.restore();
+
+/***************** Bezier Curve   ******************/
+ctx7.strokeStyle = "rgb(222,184,135)";
+ctx7.fillStyle = "rgb(245,222,179)";
+ctx7.lineWidth = 8;
+
+ctx7.beginPath()
+ctx7.moveTo(700, 260);
+//bezierCurveTo(cx1, cy1, cx2, cy2, x, y)
+ctx7.bezierCurveTo(710, 100, 10, 500, 400, 150);
+ctx7.stroke();
+
+
+/*************** Spliced Image ************/
+
+ctx7.drawImage(srcImg, 305, 582, 330, 600, 350, 100, 100, 150);
+
+} //end onload images into DOM - continued from Canvas 6
 
 //End Modernizer
 }else {
